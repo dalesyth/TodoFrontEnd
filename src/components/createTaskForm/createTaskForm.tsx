c1f947997bcb9d2207a494a1b8ef58d4bb41e376
Dale Syth <dalesyth@gmail.com>
2024-04-19 20:29:13 -0500

    Added button, alert, and progress bar

--- src/components/createTaskForm/createTaskForm.tsx
+++ src/components/createTaskForm/createTaskForm.tsx
@@ -1,5 +1,13 @@
 import React, { FC, ReactElement, useState } from 'react';
-import { Box, Typography, Stack } from '@mui/material';
+import {
+  Box,
+  Typography,
+  Stack,
+  LinearProgress,
+  Button,
+  Alert,
+  AlertTitle,
+} from '@mui/material';
 import { TaskTitleField } from './_taskTitleField';
 import { TaskDescriptionField } from './_taskDescriptionField';
 import { TaskDateField } from './_taskDateField';
@@ -24,6 +32,10 @@ export const CreateTaskForm: FC = (): ReactElement => {
       px={4}
       my={6}
     >
+      <Alert severity="success" sx={{ width: '100%', marginBottom: '16px' }}>
+        <AlertTitle>Success</AlertTitle>
+        The task has been created successfully
+      </Alert>
       <Typography mb={2} component="h2" variant="h6">
         Create A Task
       </Typography>
@@ -69,10 +81,11 @@ export const CreateTaskForm: FC = (): ReactElement => {
             ]}
           />
         </Stack>
+        <LinearProgress />
+        <Button variant="contained" size="large" fullWidth>
+          Create A Task
+        </Button>
       </Stack>
-
-      {/*Task Status*/}
-      {/*Task Priority*/}
     </Box>
   );
 };
