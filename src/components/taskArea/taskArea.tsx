a54853578a9e4c88f3eef079a68adc2dd2434801
Dale Syth <dalesyth@gmail.com>
2024-04-27 18:56:28 -0500

    Created Mutation for updating task

--- src/components/taskArea/taskArea.tsx
+++ src/components/taskArea/taskArea.tsx
@@ -3,10 +3,11 @@ import { Grid, Box, Alert, LinearProgress } from '@mui/material';
 import { format } from 'date-fns';
 import { TaskCounter } from '../taskCounter/taskCounter';
 import { Task } from '../task/task';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useMutation } from '@tanstack/react-query';
 import { sendApiRequest } from '../../helpers/sendApiRequest';
 import { ITaskApi } from './interfaces/ITaskApi';
 import { Status } from '../createTaskForm/enums/Status';
+import { IUpdateTask } from '../createTaskForm/interfaces/IUpdateTask';
 
 export const TaskArea: FC = () => {
   const { error, isLoading, data, refetch } = useQuery({
@@ -17,6 +18,12 @@ export const TaskArea: FC = () => {
         'GET',
       );
     },
+  });
+
+  // update task mutation
+  const updateTaskMutation = useMutation({
+    mutationFn: (data: IUpdateTask) =>
+      sendApiRequest('http://localhost:3200/tasks', 'PUT', data),
   });
 
   return (
